77eaa308e6acaeb85f864a6b2d773aec70b5d572
bosxch <amparocardosobosch@gmail.com>
2023-02-04 16:01:41 -0300

    changes in grill card

--- client/front/src/components/grillCard/grillCard.jsx
+++ client/front/src/components/grillCard/grillCard.jsx
@@ -4,14 +4,22 @@ import Loading from '../loading/loading'
 import Card from '../grillCard/card'
 import s from './grillCard.module.css'
 import Pagination from '../Pagination/pagination'
+import { useSelector } from "react-redux";
 
 export default function GrillCard() {
+
+    const products = useSelector((state) => state.filterProducts);
+
+
     const [loading, setLoading] = useState(true)
     const [currentPage, setCurrentPage] = useState(1)
-    const [postPage] = useState(15)
+    const [productsXPage] = useState(15)
+    
 
-    const plants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6]
-
+    // const plants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6]
+    const iLastProduct = currentPage * productsXPage;
+    const iFirstProduct = iLastProduct - productsXPage;
+    const currentProducts = products.slice(iFirstProduct, iLastProduct);
 
     useEffect(() => {
         setTimeout(() => {
@@ -19,12 +27,8 @@ export default function GrillCard() {
         }, "2500")
     })
 
-    const lastPostIndex = currentPage * postPage
-    const firstPostIndex = lastPostIndex - postPage
-    var totalPages = Math.ceil(plants.length / postPage)
-
-    function pagination() {
-        return plants.slice(firstPostIndex, lastPostIndex)
+    const pagination = (pageNumber) => {
+        setCurrentPage(pageNumber)
     }
 
     const nextPage = () => {
@@ -44,10 +48,9 @@ export default function GrillCard() {
                 <div className={s.pag}>
                     <button onClick={previousPage} className={s.prevNext}>Previous</button>
                     <Pagination
-                        totalPost={plants.length}
-                        postPerPage={postPage}
-                        setCurrentPage={setCurrentPage}
-                        currentPage={currentPage} />
+                        productsXPage={productsXPage}
+                        products={products}
+                        pagination={pagination} />
                     <button onClick={nextPage} className={s.prevNext}>Next</button>
                 </div>
             </div>
