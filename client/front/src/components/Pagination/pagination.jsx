28b6abdd3491e720d2f5e968a6458e867e7db99d
Hermes <hermeschacon@live.com.ar>
2023-02-04 22:07:36 -0300

    back&front

--- client/front/src/components/Pagination/pagination.jsx
+++ client/front/src/components/Pagination/pagination.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from '../Pagination/pagination.module.css'
 
 
-const Pagination = ({ productsXPage, pagination, products }) => {
+const Pagination = ({ productsXPage, pagination, products,currentPage }) => {
     let pages = []
 
 
