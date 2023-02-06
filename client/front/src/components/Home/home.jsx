040a89cced7b823a2b86f14a3205aa758eaaa514
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-06 17:01:38 -0300

    login and logout accessible in every routes

--- client/front/src/components/Home/home.jsx
+++ client/front/src/components/Home/home.jsx
@@ -4,23 +4,17 @@ import Carousel from "../Carousel/carousel";
 import ProdHome from "./prodHome";
 import s from "../Home/home.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Footer from '../Footer/Footer'
-import Contact from '../ContactForm/ContactForm'
-import ShopHome from '../Home/shopHome'
-import Discount from '../Discount/discount'
-import {Auth0Provider} from "@auth0/auth0-react"
+import Footer from '../Footer/Footer';
+import Contact from '../ContactForm/ContactForm';
+import ShopHome from '../Home/shopHome';
+import Discount from '../Discount/discount';
+
 
 export default function Home() {
 
     return (
         <div className={s.home}>
-            <Auth0Provider 
-                domain="dev-qbpt8eaprzg0wfvf.eu.auth0.com" 
-                clientId="JlEe3AB9FZaRWMcClP6ykipsPweRhPBg" 
-                authorizationParams={{
-                redirect_uri: window.location.origin}}>
             <Navbar />
-            </Auth0Provider>
             <Carousel />
             <div className={s.cardsH}>
                 <ProdHome id="7" name="Bromelia guzmania" style="0"/>
