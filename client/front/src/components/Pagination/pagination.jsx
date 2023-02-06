2446763590453dfe1418f845713061daba931ffe
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-02-06 12:47:45 -0500

    css modifications, cards home corrections

--- client/front/src/components/Pagination/pagination.jsx
+++ client/front/src/components/Pagination/pagination.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import s from '../Pagination/pagination.module.css'
+import s from "../Pagination/pagination.module.css";
 
+export default function Pagination({
+  productsXPage,
+  pagination,
+  plants,
+  currentPage,
+}) {
+  let pages = [];
 
-export default function Pagination({ productsXPage, pagination, plants, currentPage }) {
-    let pages = []
+  for (let i = 1; i <= Math.ceil(plants / productsXPage); i++) {
+    pages.push(i);
+  }
 
-
-    for (let i = 1; i <= Math.ceil(plants / productsXPage); i++) {
-        pages.push(i)
-    }
-
-    return (
-        <div className={s.pagination}>
-
-            {pages &&
-            pages.map((number) => (
-                <button
-                    key={number}
-                        onClick={() => pagination(number)}
-                        className={number === currentPage ? 'active' : ''}
-                        >
-                        {number}
-                    </button>
-            ))}
-
-        </div>
-    )
+  return (
+    <div className={s.pagination}>
+      {pages &&
+        pages.map((number) => (
+          <button
+            key={number}
+            onClick={() => pagination(number)}
+            className={number === currentPage ? s.actived : ""}
+          >
+            {number}
+          </button>
+        ))}
+    </div>
+  );
 }
