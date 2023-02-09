a8b5f72b9e5b096259a86bf330adb112ccbf9c36
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-02-09 14:53:42 -0500

    Button Checkout Paypal created

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -7,22 +7,29 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Shop from "./components/Shop/Shop";
 import UserBar from "./components/User/userSidebar";
 import Dashboard from "./components/dashboard/Dashboard";
-import WishlistComponent from './components/Wishlist/wishlistComponent';
+import WishlistComponent from "./components/Wishlist/wishlistComponent";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/about-us" element={<AboutUs />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/shop" element={<Shop />}></Route>
-        <Route path="/products/:productId" element={<ProductDetails />} />
-        <Route path="/user" element={<UserBar />} />
-        <Route path="/admin" element={<Dashboard />} />
-        <Route path='/wishlist' element={<WishlistComponent/>}/>
-        {/* path /user para testear componentes */}
-      </Routes>
-    </div>
+    <PayPalScriptProvider
+      options={{
+        "client-id": process.env.PAYPAL_CLIENT_ID,
+      }}
+    >
+      <div>
+        <Routes>
+          <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/products/:productId" element={<ProductDetails />} />
+          <Route path="/user" element={<UserBar />} />
+          <Route path="/admin" element={<Dashboard />} />
+          <Route path="/wishlist" element={<WishlistComponent />} />
+          {/* path /user para testear componentes */}
+        </Routes>
+      </div>
+    </PayPalScriptProvider>
   );
 }
 
