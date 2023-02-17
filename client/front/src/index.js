e21a07c5616dcbaad3649d668647352883d1a907
Ignacio <nosoyignacio@gmail.com>
2023-02-17 14:22:34 -0300

    del/bootstrap

--- client/front/src/index.js
+++ client/front/src/index.js
@@ -11,7 +11,6 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import Auth0ProviderHistory from "./auth0-provider-history";
 import Toaster from './Toaster'
 import ErrorBoundary from "./ErrorBoundary";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 axios.defaults.baseURL = "https://pf-henry-production-4976.up.railway.app/";
 
