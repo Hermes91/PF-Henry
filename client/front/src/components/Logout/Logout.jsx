2d54efe9029f6e0092be224fe7cac09cb136f736
Ignacio <nosoyignacio@gmail.com>
2023-02-17 06:07:13 -0300

    feat/someIcons

--- client/front/src/components/Logout/Logout.jsx
+++ client/front/src/components/Logout/Logout.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useAuth0 } from '@auth0/auth0-react'
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function LogoutButton({className}) {
     const { logout } = useAuth0() 
 
-    return <button className={className} onClick={() => logout({returnTo: window.location.origin})}>Logout</button>
+    return <LogoutIcon className={className} onClick={() => logout({returnTo: window.location.origin})}/>
 }
