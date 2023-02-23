227510891481c1f7631cdcc97a8cfc6cf507780d
bosxch <amparocardosobosch@gmail.com>
2023-02-23 00:26:48 -0300

    bye link

--- client/front/src/components/Map/MapInteractive.jsx
+++ client/front/src/components/Map/MapInteractive.jsx
@@ -30,10 +30,7 @@ const MapInteractive = ({ positionDetail }) => {
        
             <Marker position={mapCenter} icon={MarkerIcon}>
               <Popup position={mapCenter}>
-                <div className={s.navlink} onClick={function() {
-                 const newPest = window.open('https://www.google.com/maps/place/3700+SE+Washington+St,+Portland,+OR+97214,+EE.+UU./@45.5178241,-122.6284546,16z/data=!4m5!3m4!1s0x5495a0eb68828817:0x3fd4af99246d5b58!8m2!3d45.518586!4d-122.6258284?hl=en', '_blank')
-                  newPest.focus()
-                }}>
+                <div className={s.navlink}>
                   <h3 className={s.title}>
                     Vivero Henry
                   </h3>
